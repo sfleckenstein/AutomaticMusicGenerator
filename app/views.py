a238dc6deaac80938aed9703526b40acb3e07875
Ross Nanopoulos <rdn21@case.edu>
2014-03-21 19:13:53 -0400

    Updated UI views.

--- app/views.py
+++ app/views.py
@@ -4,4 +4,15 @@ from forms import ChoiceForm
 
 @app.route('/')
 def index():
-    return render_template("index.html", title='Intelligent Music Generator', form=ChoiceForm())
+    return render_template("index.html", form=ChoiceForm())
+
+@app.route('/begin')
+def begin():
+    #TODO: Logic for displaying to user
+    return render_template("begin.html")
+
+
+@app.route('/compose')
+def compose():
+    #TODO: Logic for composing goes here
+    return render_template("compose.html")
