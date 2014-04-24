fc21d4b4cfafabc8ed89dba12007ef310d5b62fd
ross <ross@luna.(none)>
2014-04-24 10:26:34 -0400

    Added GUI, but need to get actual options from The Echo Nest; extracted durations from notes; durations use the Trainer.

---
+++ app/Creator.py
@@ -0,0 +1,57 @@
+import time
+import sys
+
+from pyechonest import song, config
+
+import LearningAgent.BarLearner as BarLearner
+import LearningAgent.NoteLearner as NoteLearner
+import LearningAgent.DurationLearner as DurationLearner
+
+from config import ECHO_NEST_API_KEY
+from Composer.Composer import compose
+from LearningAgent.DataCollector import collect_data
+from LearningAgent.SongSelector import rank_songs
+
+
+config.ECHO_NEST_API_KEY=ECHO_NEST_API_KEY
+
+def create():
+    print('Starting search')
+    songs = song.search(style='folk',
+                        max_tempo=150,
+                        min_tempo=100,
+                        results=1)
+
+    print('Search returned')
+    start = time.time()
+    
+    song_ids = []
+    for track in songs:
+        song_ids.append(track.id)
+    
+    # TODO get the real tempo
+    tempo = 120 * 4 
+
+    print('Collecting data')
+    songs_data = collect_data(song_ids)
+    print('Data collected')
+
+    print('Ranking songs')
+    rank_songs(songs_data, tempo) 
+    print('Songs ranked')
+
+    print('Training models')
+    (bar_model, bar_alphabet) = BarLearner.train_model(songs_data)
+    print str(bar_model), str(bar_alphabet)
+    (note_models, note_alphabet) = NoteLearner.train_model(songs_data)
+    print str(note_models), str(note_alphabet)
+    (duration_model, duration_alphabet) = DurationLearner.train_model(songs_data, tempo)
+    print str(duration_model), str(duration_alphabet)
+    print('Models trained')
+
+    print('Composing')
+    compose(bar_model, note_models, duration_model, bar_alphabet, note_alphabet, duration_alphabet)
+    print('Done composing')
+    end = time.time()
+    
+    print('Total time: {}'.format(end - start))
