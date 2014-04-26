a96653998e242356079cc51831d828a8c76ed4fe
ross <ross@luna.(none)>
2014-04-25 23:36:37 -0400

    Added in choices, created worker thread, music now plays in the application.

--- app/Creator.py
+++ app/Creator.py
@@ -1,7 +1,7 @@
-import time
-import sys
+import sys,time
 
 from pyechonest import song, config
+from PyQt4 import QtCore
 
 import LearningAgent.BarLearner as BarLearner
 import LearningAgent.NoteLearner as NoteLearner
@@ -12,46 +12,60 @@ from Composer.Composer import compose
 from LearningAgent.DataCollector import collect_data
 from LearningAgent.SongSelector import rank_songs
 
-
 config.ECHO_NEST_API_KEY=ECHO_NEST_API_KEY
 
-def create(style, max_tempo=None, min_tempo=None, key=None, time_sig=None):
-    print('Starting search')
-    songs = song.search(style=style,
-                        max_tempo=max_tempo,
-                        min_tempo=min_tempo,
-                        results=1)
+class CreatorThread(QtCore.QObject):
+    def __init__(self, style, max_tempo, min_tempo, key):
+        QtCore.QObject.__init__(self)
+        self.style = style
+        self.max_tempo = max_tempo
+        self.min_tempo = min_tempo
+        self.key = key
 
-    print('Search returned')
-    start = time.time()
+    def process(self):
+        self.create(self.style, self.max_tempo, self.min_tempo, self.key)
+        self.finished.emit()
+
+    finished = QtCore.pyqtSignal()
+
+    def create(self, style, max_tempo=None, min_tempo=None, key=None):
+        print('Starting search')
+        songs = song.search(style=style,
+                            max_tempo=max_tempo,
+                            min_tempo=min_tempo,
+                            key=key,
+                            results=1)
+
+        print('Search returned')
+        start = time.time()
     
-    song_ids = []
-    for track in songs:
-        song_ids.append(track.id)
+        song_ids = []
+        for track in songs:
+            song_ids.append(track.id)
     
-    # TODO get the real tempo
-    tempo = 120 * 4 
+        # TODO get the real tempo
+        tempo = 120 * 4 
 
-    print('Collecting data')
-    songs_data = collect_data(song_ids)
-    print('Data collected')
+        print('Collecting data')
+        songs_data = collect_data(song_ids)
+        print('Data collected')
 
-    print('Ranking songs')
-    rank_songs(songs_data, tempo) 
-    print('Songs ranked')
+        print('Ranking songs')
+        rank_songs(songs_data, tempo) 
+        print('Songs ranked')
 
-    print('Training models')
-    (bar_model, bar_alphabet) = BarLearner.train_model(songs_data)
-    print str(bar_model), str(bar_alphabet)
-    (note_models, note_alphabet) = NoteLearner.train_model(songs_data)
-    print str(note_models), str(note_alphabet)
-    (duration_model, duration_alphabet) = DurationLearner.train_model(songs_data, tempo)
-    print str(duration_model), str(duration_alphabet)
-    print('Models trained')
+        print('Training models')
+        (bar_model, bar_alphabet) = BarLearner.train_model(songs_data)
+        print str(bar_model), str(bar_alphabet)
+        (note_models, note_alphabet) = NoteLearner.train_model(songs_data)
+        print str(note_models), str(note_alphabet)
+        (duration_model, duration_alphabet) = DurationLearner.train_model(songs_data, tempo)
+        print str(duration_model), str(duration_alphabet)
+        print('Models trained')
 
-    print('Composing')
-    compose(bar_model, note_models, duration_model, bar_alphabet, note_alphabet, duration_alphabet)
-    print('Done composing')
-    end = time.time()
+        print('Composing')
+        compose(bar_model, note_models, duration_model, bar_alphabet, note_alphabet, duration_alphabet)
+        print('Done composing')
+        end = time.time()
     
-    print('Total time: {}'.format(end - start))
+        print('Total time: {}'.format(end - start))
--- app/ExtendedComboBox.py
+++ app/ExtendedComboBox.py
@@ -2,47 +2,45 @@ import sys
 from PyQt4.QtGui import QComboBox, QApplication, QCompleter, QSortFilterProxyModel, QStandardItemModel, QStandardItem
 from PyQt4.QtCore import Qt
 
-class ExtendedComboBox( QComboBox ):
-    def __init__( self,  parent = None):
-        super( ExtendedComboBox, self ).__init__( parent )
+class ExtendedComboBox(QComboBox):
+    """ Based off the extension of the combo box from below:
+        http://stackoverflow.com/questions/4827207/how-do-i-filter-the-pyqt-qcombobox-items-based-on-the-text-input
+    """
+    def __init__(self, parent = None):
+        super(ExtendedComboBox, self).__init__(parent)
 
-        self.setFocusPolicy( Qt.StrongFocus )
-        self.setEditable( True )
+        self.setFocusPolicy(Qt.StrongFocus)
+        self.setEditable(True)
 
-        self.setEditable( True )
-        self.completer = QCompleter( self )
+        self.setEditable(True)
+        self.completer = QCompleter(self)
 
         # always show all completions
-        self.completer.setCompletionMode( QCompleter.UnfilteredPopupCompletion )
-        self.pFilterModel = QSortFilterProxyModel( self )
-        self.pFilterModel.setFilterCaseSensitivity( Qt.CaseInsensitive )
+        self.completer.setCompletionMode(QCompleter.PopupCompletion)
+        self.pFilterModel = QSortFilterProxyModel(self)
+        self.pFilterModel.setFilterCaseSensitivity(Qt.CaseInsensitive)
 
+        self.completer.setPopup(self.view())
+        self.setCompleter(self.completer)
 
-
-        self.completer.setPopup( self.view() )
-
-
-        self.setCompleter( self.completer )
-
-
-        self.lineEdit().textEdited[unicode].connect( self.pFilterModel.setFilterFixedString )
+        self.lineEdit().textEdited[unicode].connect(self.pFilterModel.setFilterFixedString)
         self.completer.activated.connect(self.setTextIfCompleterIsClicked)
 
-    def setModel( self, model ):
+    def setModel(self, model):
         super(ExtendedComboBox, self).setModel( model )
         self.pFilterModel.setSourceModel( model )
         self.completer.setModel(self.pFilterModel)
 
-    def setModelColumn( self, column ):
-        self.completer.setCompletionColumn( column )
-        self.pFilterModel.setFilterKeyColumn( column )
-        super(ExtendedComboBox, self).setModelColumn( column )
+    def setModelColumn(self, column):
+        self.completer.setCompletionColumn(column)
+        self.pFilterModel.setFilterKeyColumn(column)
+        super(ExtendedComboBox, self).setModelColumn(column)
 
 
-    def view( self ):
+    def view(self):
         return self.completer.popup()
 
-    def index( self ):
+    def index(self):
         return self.currentIndex()
 
     def setTextIfCompleterIsClicked(self, text):
